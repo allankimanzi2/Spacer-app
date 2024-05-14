96c9dbb81bbf838e416e295e53e2e6bf392c7762
allankimanzi2 <allankim457@gmail.com>
2024-05-14 16:05:37 +0300

    added frontend configurations, styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-export default function App() {
+import React from 'react';
+import './App.css';
+import Admin from './components/Admin';
+import User from './components/User';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="App">
+      <header className="bg-gray-800 text-white p-4">
+        <h1 className="text-3xl">Space Booking Platform</h1>
+      </header>
+      <div className="my-4 text-center">
+        <input 
+          type="text" 
+          placeholder="Search for spaces" 
+          className="p-2 border rounded w-1/2"
+        />
+        <button className="p-2 bg-blue-500 text-white rounded ml-2">Search</button>
+      </div>
+      <Admin />
+      <User />
+    </div>
+  );
 }
+
+export default App;
